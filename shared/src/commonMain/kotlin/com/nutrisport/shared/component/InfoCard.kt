e766153572443c7cbf4d173db341fd6fbf5d54de
Taxist Samael <yakiv.bondar@gmail.com>
2026-01-20 17:25:55 +0200

    ui fix

--- shared/src/commonMain/kotlin/com/nutrisport/shared/component/InfoCard.kt
+++ shared/src/commonMain/kotlin/com/nutrisport/shared/component/InfoCard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.Resources
@@ -44,8 +46,10 @@ fun InfoCard(
     )
     Spacer(modifier = Modifier.height(4.dp))
     Text(
+      modifier = Modifier.fillMaxWidth(),
       text = subtitle,
       fontSize = FontSize.REGULAR,
+      textAlign = TextAlign.Center,
     )
   }
 }

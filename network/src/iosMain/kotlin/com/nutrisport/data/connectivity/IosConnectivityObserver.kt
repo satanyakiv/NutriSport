92053b48edd68bd43c8ca0198f037d8703193aee
Yakiv <yakiv.bondar@gmail.com>
2026-03-27 18:24:52 +0200

    Fix missing nw_path_monitor_create import in IosConnectivityObserver

--- network/src/iosMain/kotlin/com/nutrisport/data/connectivity/IosConnectivityObserver.kt
+++ network/src/iosMain/kotlin/com/nutrisport/data/connectivity/IosConnectivityObserver.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import platform.Network.nw_path_get_status
 import platform.Network.nw_path_monitor_cancel
+import platform.Network.nw_path_monitor_create
 import platform.Network.nw_path_monitor_set_queue
 import platform.Network.nw_path_monitor_set_update_handler
 import platform.Network.nw_path_monitor_start
